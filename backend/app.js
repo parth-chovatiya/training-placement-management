d5b2c7073ceed2c1e5ee3171142d037ccd0b6460
parth-chovatiya <parthchovatiya3@gmail.com>
2021-06-20 13:18:59 +0530

    db connected

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,13 @@
-const dotenv = require("dotenv");
 const express = require("express");
+const morgan = require("morgan");
 const connectDB = require("./config/db");
-const cookieParser = require("cookie-parser");
 
 const app = express();
 connectDB();
-app.use(cookieParser());
 
-dotenv.config({ path: "./config.env" }); // give path of config.env file
-// const User = require('./model/userSchema')
+app.use(express.json({extended:false}));
+app.use(morgan('dev'))
 
-app.use(express.json());
-
-const port = process.env.PORT || 4000;
 
 app.get("/", (req, res) => res.send("API Running"));
 
@@ -20,6 +15,9 @@ app.get("/", (req, res) => res.send("API Running"));
 // app.use("/api/student", require("./router/api/student"));
 app.use("/api/company", require("./router/api/company"));
 
-app.listen(port, () => {
-  console.log(`Server is running on ${port}`);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`.bgBlack.bold.italic);
 });
