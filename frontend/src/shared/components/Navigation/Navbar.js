bcf9a84564fa039c53a37919d1acc818686e2338
parth-chovatiya <parthchovatiya3@gmail.com>
2021-06-24 18:02:15 +0530

    navbar modified

--- frontend/src/shared/components/Navigation/Navbar.js
+++ frontend/src/shared/components/Navigation/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { Button } from './Button';
 import './Navbar.css'
+import Cookie from 'js-cookie'
 
 function Navbar() {
 
@@ -10,9 +11,10 @@ function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
+  
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
+  
   const showButton = () => {
     if(window.innerWidth <= 960){
       setButton(false);
@@ -20,8 +22,17 @@ function Navbar() {
       setButton(true);
     }
   };
-
   useEffect(() => {
+    const lg = Cookie.get('login')
+    console.log(lg)
+    if(lg==='student'){
+      setLoginStudent(true)
+      setLoginCompany(false)
+    }
+    else if(lg==='company'){
+      setLoginCompany(true)
+      setLoginStudent(false)
+    }
     showButton();
   }, []);
 
@@ -45,11 +56,26 @@ function Navbar() {
                 Home
               </Link>
             </li>
-            <li className='nav-item'>
+            {loginStudent && (<li className='nav-item'>
+              <Link to='/student/dashboard' className='nav-links' onClick={closeMobileMenu}>
+                Dashboard
+              </Link>
+            </li>)}
+            {loginCompany && (<li className='nav-item'>
+              <Link to='/company/dashboard' className='nav-links' onClick={closeMobileMenu}>
+                Dashboard
+              </Link>
+            </li>)}
+            {(!loginStudent && !loginCompany) && (<li className='nav-item'>
               <Link to='/login' className='nav-links' onClick={closeMobileMenu}>
                 Login
               </Link>
-            </li>
+            </li>)}
+            {(loginStudent || loginCompany)&& (<li className='nav-item'>
+              <Link to='/logout' className='nav-links' onClick={closeMobileMenu}>
+                Logout
+              </Link>
+            </li>)}
             {/* <li className='nav-item'>
               <Link to='/student/login' className='nav-links' onClick={closeMobileMenu}>
                 Student Login
