eb0ea4433cfc24c25da42e0582caec1dbb69a4bb
parth-chovatiya <parthchovatiya3@gmail.com>
2021-07-02 16:24:59 +0530

    Training & Placement Page Content modified

--- frontend/src/Home/pages/TrainingPlacement.js
+++ frontend/src/Home/pages/TrainingPlacement.js
@@ -6,20 +6,23 @@ import Companies_Visited_BarChart from "../components/Companies_Visited_BarChart
 import Highest_Packege_LineChart from "../components/Highest_Packege_LineChart";
 import Average_Package_LineChart from "../components/Average_Package_LineChart";
 
+import './TrainingPlacecment.css'
+
 const TrainingPlaccement = () => {
   return (
     <>
-      <h1>Training and Placement Cell</h1>
-      <hr />
-      <br />  
-      BVM, a premier engineering college, is considered to be top 1% among all engineering colleges in Gujarat. Since inception, BVM has a record of best Placement all over Gujarat. Around 35+companies, including MNCs like Infosys, Essar, TCS, L&T, ABB, Torrent Power, Seimens, etc.. visit BVM every year for placement.
-      <br /> <br /> <br />
-      Mr. Mehul Patel    [I/C Training And Placement]
-      <br /> <br /> <br />
-      Dr. Mehul Shah    [Assistant Training And Placement]
-      <br /> <br /> <br />
-      Contact:+91 9978997088/ +91 9909529395
-      <br /> <br /> <br />
+      <div className="details">
+        <h1>Training and Placement Cell</h1>
+        <br />  
+        BVM, a premier engineering college, is considered to be top 1% among all engineering colleges in Gujarat. Since inception, BVM has a record of best Placement all over Gujarat. Around 35+companies, including MNCs like Infosys, Essar, TCS, L&T, ABB, Torrent Power, Seimens, etc.. visit BVM every year for placement.
+        <br /> <br /> <br />
+        Mr. Mehul Patel    [I/C Training And Placement]
+        <br /> <br /> <br />
+        Dr. Mehul Shah    [Assistant Training And Placement]
+        <br /> <br /> <br />
+        Contact:+91 9978997088/ +91 9909529395
+        <br /> <br /> <br />
+      </div>
       
       <div className="chart">
         <div className="firstRow">
